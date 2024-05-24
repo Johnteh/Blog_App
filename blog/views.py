9ee802c0f5ee44673fc545eecb14113539d477f3
Johnteh <miirijohn08@gmail.com>
2024-05-24 09:15:18 +0000

    django project for a blog app

---
+++ blog/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+posts = [
+    {
+        'author': 'John miiri',
+        'title':'Blog post 1',
+        'content': 'First post content',
+        'date_posted': 'May 24, 2018'
+    },
+    {
+        'author': 'John miiri',
+        'title':'Blog post 2',
+        'content': 'Second post content',
+        'date_posted': 'May 24, 2018'
+    }
+]
+# Create your views here.
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html') 
