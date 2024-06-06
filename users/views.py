ebb414a2afbac26731d45c37fa8bf4c68c36c3a2
Johnteh <miirijohn08@gmail.com>
2024-06-06 19:50:24 +0000

    added login functionality and logout functionality

--- users/views.py
+++ users/views.py
@@ -9,8 +9,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your account as succesfully created. Please log in')
+            return redirect('login')
     else:
         form = UserRegisterForm(request.POST)
 
