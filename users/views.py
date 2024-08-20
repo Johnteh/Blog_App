6fbd5420482970a079947a272c8e39e1f41ea942
Johnteh <miirijohn08@gmail.com>
2024-08-20 18:14:08 +0000

    resized images and added display of profile pics alongside the posts

--- users/views.py
+++ users/views.py
@@ -10,7 +10,7 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account as succesfully created. Please log in')
+            messages.success(request, f'Account succesfully created for {username}!')
             return redirect('login')
     else:
         form = UserRegisterForm(request.POST)
