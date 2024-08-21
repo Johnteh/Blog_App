7e73c00594577fdacd52d56e4d00c1b7d446b758
Johnteh <miirijohn08@gmail.com>
2024-08-21 19:48:06 +0000

    added CRUD functionality

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
+from .views import PostlistView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'blog-home'),
+    path('', PostlistView.as_view(), name = 'blog-home'),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post-detail"),
+    path("post/new/", PostCreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/update", PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete", PostDeleteView.as_view(), name="post-delete"),
     path('about/', views.about, name = 'blog-about'),
 ]
+
+
+#<app>/<model>_<viewtype>.html
